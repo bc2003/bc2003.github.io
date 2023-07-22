95713d1a72d7dc30add71c71b96a5585976b960b
bc2003 <bc2003@student.ubc.ca>
2023-07-22 14:56:49 -0700

    done projects template

--- main.js
+++ main.js
@@ -3,3 +3,63 @@ const card = document.querySelector(".card__inner");
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
 });
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstProjectWidth = carousel.querySelector(".project").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+// Get the number of projects that can fit in the carousel at once (aim for 3 usually)
+let projPerView = Math.round(carousel.offsetWidth / firstProjectWidth);
+
+// Insert copies of the last few projects to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-projPerView).reverse().forEach(project => {
+    carousel.insertAdjacentHTML("afterbegin", project.outerHTML);
+});
+
+// Insert copies of the first few projects to end of carousel for infinite scrolling
+carouselChildrens.slice(0, projPerView).forEach(project => {
+    carousel.insertAdjacentHTML("beforeend", project.outerHTML);
+});
+
+// // Scroll the carousel at appropriate postition to hide first few duplicate projects
+// carousel.classList.add("no-transition");
+// carousel.scrollLeft = carousel.offsetWidth;
+// carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstProjectWidth : firstProjectWidth;
+    });
+});
+
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+// const autoPlay = () => {
+//     if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+//     // Autoplay the carousel after every 2500 ms
+//     timeoutId = setTimeout(() => carousel.scrollLeft += firstProjectWidth, 2500);
+// }
+// autoPlay();
+
+carousel.addEventListener("scroll", infiniteScroll);
+// wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+// wrapper.addEventListener("mouseleave", autoPlay);
